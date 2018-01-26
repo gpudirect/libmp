9e938ef5fa246d9d7cee7f889f2e067ba3111c19
e-ago <elena.ago@gmail.com>
2018-01-26 16:04:48 +0100

    Minor changes to libmp.mk

--- libmp.mk
+++ libmp.mk
@@ -1,15 +1,14 @@
 # **********************************************************************
 # Install path configuration
 # **********************************************************************
-# PREFIX = /path/where/install/libmp
-# it can be also set as environment variable
+# PREFIX = /path/to/libmp
 # **********************************************************************
 PREFIX = $(HOME)/libmp
 
 # **********************************************************************
 # LibGDSync configuration
 # **********************************************************************
-# GDSYNC_BUILD = 1 to build the libgdsync submodule
+# GDSYNC_BUILD = 1 build the libgdsync submodule
 # GDSYNC_BUILD = 0 otherwise
 # ----------------------------------------------------------------------
 # GDSYNC_ENABLE = /path/to/libgdsync to enable LibGDSync inside LibMP
@@ -41,21 +40,3 @@ CUDA_ENABLE = /usr/local/cuda-9.0
 # N.B. Currently libmp support only MPI as TL!
 # **********************************************************************
 MPI_ENABLE = /opt/openmpi/v1.10.2/cuda7.5
-
-#------------------------------------------------------------------------------
-# build tunables
-#------------------------------------------------------------------------------
-
-CXX:=g++
-CC:=gcc
-NVCC:=nvcc
-LD:=g++
-COMMON_CFLAGS:=-O2
-CPPFLAGS+=-DGDS_USE_EXP_INIT_ATTR -DGDS_OFED_HAS_PEER_DIRECT_ASYNC
-NVCC_ARCHS:=-gencode arch=compute_35,code=sm_35
-NVCC_ARCHS+=-gencode arch=compute_50,code=compute_50
-NVCC_ARCHS+=-gencode arch=compute_60,code=compute_60
-NVCC_ARCHS+=-gencode arch=compute_70,code=compute_70
-
-# static or dynamic
-LIBS_TYPE=static
